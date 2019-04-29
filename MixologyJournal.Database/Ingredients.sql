cd490df6d8ab1495cd631bf01fe66148a3cc7563
David M <656337+davidov541@users.noreply.github.com>
2019-04-28 23:08:32 -0500

    Adding projects for the database and the web service.
    Both are now deployed.

---
+++ MixologyJournal.Database/Ingredients.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Ingredients]
+(
+	Id BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	RecipeId BIGINT NOT NULL,
+	Name VARCHAR(100) NOT NULL, 
+    [Brand] VARCHAR(200) NULL, 
+    [AmountUnit] VARCHAR(50) NOT NULL, 
+    [AmountQuantity] DECIMAL(5, 2) NOT NULL, 
+    CONSTRAINT [FK_Ingredients_ToRecipe] FOREIGN KEY (RecipeId) REFERENCES Recipes(Id),
+
+)
