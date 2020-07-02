2a8eea82339baa825e0a030171f6cf180c999c9b
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-02 18:23:12 -0500

    Upping the version to 1.0.6.

--- MixologyJournalApp/MixologyJournalApp.Android/Properties/AssemblyInfo.cs
+++ MixologyJournalApp/MixologyJournalApp.Android/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MixologyJournalApp.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -28,8 +28,8 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
