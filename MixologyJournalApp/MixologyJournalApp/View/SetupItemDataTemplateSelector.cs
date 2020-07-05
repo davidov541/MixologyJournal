cd29071764a5f1e99bfdfc8c584b0bc6939ade29
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-04 22:07:40 -0500

    Switching to using data template selector for setup carosel.

---
+++ MixologyJournalApp/MixologyJournalApp/View/SetupItemDataTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using MixologyJournalApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MixologyJournalApp.View
+{
+    public class SetupItemDataTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate DescriptionTemplate { get; set; }
+        public DataTemplate LoginTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            switch (((SetupPageItem)item).Type)
+            {
+                case SetupPageItem.ItemType.Description:
+                    return DescriptionTemplate;
+                case SetupPageItem.ItemType.Login:
+                    return LoginTemplate;
+            }
+            return null;
+        }
+    }
+}
