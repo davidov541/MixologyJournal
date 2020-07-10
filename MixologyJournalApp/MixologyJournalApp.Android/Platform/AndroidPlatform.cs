d00d37515fcc68324f2748133fd8358aaee5ee25
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-10 16:27:57 -0500

    Adding local login method.

--- MixologyJournalApp/MixologyJournalApp.Android/Platform/AndroidPlatform.cs
+++ MixologyJournalApp/MixologyJournalApp.Android/Platform/AndroidPlatform.cs
@@ -1,37 +1,22 @@
 ﻿using MixologyJournalApp.Platform;
-using System.ComponentModel;
 
 namespace MixologyJournalApp.Droid.Platform
 {
     public class AndroidPlatform : IPlatform
     {
-        private readonly AlertDialogFactory _factory;
-        public IAlertDialogFactory AlertDialogFactory
-        {
-            get
-            {
-                return _factory;
-            }
-        }
+        public IAlertDialogFactory AlertDialogFactory { get; }
 
-        private readonly BackendManager _backend;
-        public IBackend Backend
-        {
-            get
-            {
-                return _backend;
-            }
-        }
+        public IBackend Backend { get; }
 
         public AuthenticationManager Authentication { get; }
 
         internal AndroidPlatform(MainActivity context)
         {
-            Authentication = new AuthenticationManager(new Auth0LoginMethod(context));
+            Authentication = new AuthenticationManager(new Auth0LoginMethod(context), new LocalLoginMethod());
 
-            _factory = new AlertDialogFactory(context);
+            AlertDialogFactory = new AlertDialogFactory(context);
 
-            _backend = new BackendManager(Authentication);
+            Backend = new BackendManager(Authentication);
         }
     }
 }
---
+++ MixologyJournalApp/MixologyJournalApp.Android/Platform/LocalLoginMethod.cs
@@ -0,0 +1,104 @@
+﻿using MixologyJournalApp.Platform;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MixologyJournalApp.Droid.Platform
+{
+    public class LocalLoginMethod : ILoginMethod
+    {
+        public string Name
+        {
+            get
+            {
+                return "Local Android Account";
+            }
+        }
+
+        public String Icon
+        {
+            get
+            {
+                return "icon_auth0";
+            }
+        }
+
+        public String BackgroundColor
+        {
+            get
+            {
+                return "#F8F8F8";
+            }
+        }
+
+        public String ForegroundColor
+        {
+            get
+            {
+                return "#000000";
+            }
+        }
+
+        private Boolean _isLoggedIn = false;
+        public Boolean IsLoggedIn
+        {
+            get
+            {
+                return _isLoggedIn;
+            }
+            private set
+            {
+                _isLoggedIn = value;
+                OnPropertyChanged(nameof(IsLoggedIn));
+            }
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new Command(Login);
+            }
+        }
+
+        public ICommand LogoffCommand
+        {
+            get
+            {
+                return new Command(Logoff);
+            }
+        }
+
+        public User CurrentUser
+        {
+            get;
+            private set;
+        }
+
+        public LocalLoginMethod()
+        {
+        }
+
+        public async Task Init(bool setupMode)
+        {
+            IsLoggedIn = true;
+        }
+
+        private async void Login()
+        {
+            IsLoggedIn = true;
+        }
+
+        private void Logoff()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
