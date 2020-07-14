f3643720113ee416e6366bc389f987a7c15517a3
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-14 16:20:27 -0500

    We now have custom colors for the recipe and drink cards, to differentiate them.

---
+++ MixologyJournalApp/MixologyJournalApp/View/Controls/CreationTypeToBackgroundColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using MixologyJournalApp.ViewModel;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MixologyJournalApp.View.Controls
+{
+    public class CreationTypeToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            CreationType type = (CreationType)value;
+            Object color = null;
+            switch (type)
+            {
+                case CreationType.Recipe:
+                    App.Current.Resources.TryGetValue("RecipeCardBackground", out color);
+                    break;
+                case CreationType.Drink:
+                    App.Current.Resources.TryGetValue("DrinkCardBackground", out color);
+                    break;
+                default:
+                    throw new NotSupportedException("Invalid creation type: " + type.ToString());
+            }
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
