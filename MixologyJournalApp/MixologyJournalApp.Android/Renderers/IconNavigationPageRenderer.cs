9b00651a316ff592783b5f2dfb40e870b9b329c0
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-22 23:48:27 -0500

    Skinning the header of the creation pages.

---
+++ MixologyJournalApp/MixologyJournalApp.Android/Renderers/IconNavigationPageRenderer.cs
@@ -0,0 +1,48 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Support.V7.Widget;
+using MixologyJournalApp.Droid.Renderers;
+using MixologyJournalApp.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(RootPage), typeof(IconNavigationPageRenderer))]
+namespace MixologyJournalApp.Droid.Renderers
+{
+    public class IconNavigationPageRenderer : MasterDetailPageRenderer
+    {
+        private Context _context;
+
+        public IconNavigationPageRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+        {
+            base.OnLayout(changed, left, top, right, bottom);
+
+            Page currentPage = ((NavigationPage)((MasterDetailPage)Application.Current.MainPage).Detail).CurrentPage;
+
+            if (currentPage is CreateDrinkPage || currentPage is CreateRecipePage)
+            {
+                Bitmap srcBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.cancel_48);
+                int srcWidth = srcBitmap.Width;
+                int srcHeight = srcBitmap.Height;
+                int dstWidth = (int)(srcWidth * 0.6f);
+                int dstHeight = (int)(srcHeight * 0.6f);
+                Bitmap dstBitmap = Bitmap.CreateScaledBitmap(srcBitmap, dstWidth, dstHeight, true);
+
+                SetNavigationIcon(dstBitmap);
+            }
+        }
+        private void SetNavigationIcon(Bitmap icon)
+        {
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            AppCompatImageButton navIcon = toolbar.NavigationIcon.Callback as AppCompatImageButton;
+
+            navIcon?.SetImageBitmap(icon);
+        }
+    }
+}
