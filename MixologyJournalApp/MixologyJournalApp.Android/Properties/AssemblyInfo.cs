4c579e40b192b36c2abc6aee2cb5492b79a338af
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-29 22:59:28 -0500

    Updating version to 1.0.0

--- MixologyJournalApp/MixologyJournalApp.Android/Properties/AssemblyInfo.cs
+++ MixologyJournalApp/MixologyJournalApp.Android/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
