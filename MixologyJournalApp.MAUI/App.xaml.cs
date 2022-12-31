4e824f621c735887eb881305f4615c47a79f0bab
David McGinnis <656337+davidov541@users.noreply.github.com>
2022-12-31 11:50:03 -0600

    Initial creation of MAUI project.

---
+++ MixologyJournalApp.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MixologyJournalApp.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MixologyJournalApp.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MixologyJournalApp.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
